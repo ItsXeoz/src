4a577706915064081cffddabd6dfed00a89663c9
Abidzar Giffari <119671026+ItsXeoz@users.noreply.github.com>
2025-06-23 01:06:42 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,21 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+           input: [
+  'resources/css/main.css',
+  'resources/vendor/bootstrap/css/bootstrap.min.css',
+  'resources/vendor/bootstrap-icons/bootstrap-icons.css',
+  'resources/vendor/glightbox/css/glightbox.min.css',
+  'resources/vendor/swiper/swiper-bundle.min.css',
+  'resources/vendor/bootstrap/js/bootstrap.bundle.min.js',
+  'resources/vendor/php-email-form/validate.js',
+  'resources/vendor/purecounter/purecounter_vanilla.js',
+  'resources/vendor/glightbox/js/glightbox.min.js',
+  'resources/vendor/swiper/swiper-bundle.min.js',
+  'resources/vendor/imagesloaded/imagesloaded.pkgd.min.js',
+  'resources/vendor/isotope-layout/isotope.pkgd.min.js',
+  'resources/js/main.js',
+],
             refresh: true,
         }),
     ],
