4ca313c4b4ff3bf528debb7006f17785ba4850bb
AkuXeoz <abidzar.fathoni321@gmail.com>
2025-06-23 02:17:42 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,13 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
-export default defineConfig();
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                
+            ],
+            refresh: true,
+        }),
+    ],
+});
