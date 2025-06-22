6c46dcf5f7cb86940dbb5afc4e88b6777a753361
Abidzar Giffari <119671026+ItsXeoz@users.noreply.github.com>
2025-06-22 17:54:37 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production')) {
+        Log::info('Running migration from AppServiceProvider...');
+        Artisan::call('migrate', ['--force' => true]);
+    }
     }
 }
